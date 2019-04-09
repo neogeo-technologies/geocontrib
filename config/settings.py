a13290f447c49b70bbdbc65cfa63c82a131a21dd
Salima El Mokhtari <selmokhtari@neogeo.fr>
2019-04-09 10:40:55 +0200

    Mise à jour settings

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nc(f0fdnp)w5h5t$)e=37q=^zktk)!=hhk%f+e&w^osqf2^dg^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,11 +79,11 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'signalement',
-        'USER': 'salima',
+        'NAME': '',
+        'USER': '',
         'PASSWORD' : '',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'HOST': '',
+        'PORT': ''
 
     }
 }
