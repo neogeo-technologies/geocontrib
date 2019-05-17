e003082dd2bbc4685cbd22ce8d60e23fec090ae1
Salima El Mokhtari <selmokhtari@neogeo.fr>
2019-05-17 15:34:30 +0200

    Bug fix + mise à jour requirements

--- collab/templatetags/app_filters.py
+++ collab/templatetags/app_filters.py
@@ -26,13 +26,16 @@ def feature_val_display(val):
 
 @register.filter
 def get_timestamp(timestamp):
-    try:
+    if timestamp:
         # ts = """{nbjours} Jours {nbmin} Minutes {nbsec} Secondes """.format(
         #    nbjours=timestamp.days,
         #    nbmin=timestamp.seconds // 3600,
         #    nbsec=timestamp.seconds // 60 % 60)
-        ts = """{nbjours}""".format(
-           nbjours=timestamp.days)
-        return ts
-    except ValueError:
-        return None
+        try:
+            ts = """{nbjours}""".format(
+               nbjours=timestamp.days)
+            return ts
+        except Exception as e:
+                return ""
+    else:
+        return ""
