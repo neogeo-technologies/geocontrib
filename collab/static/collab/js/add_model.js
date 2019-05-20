e4d57ca4c2d8a8eb6fa812e392eef1bdbe9efad7
Salima El Mokhtari <selmokhtari@neogeo.fr>
2019-05-20 17:15:09 +0200

    Bug Fix

--- collab/static/collab/js/add_model.js
+++ collab/static/collab/js/add_model.js
@@ -3,76 +3,98 @@ $('.ui.selection.dropdown')
 ;
 
 
-$(document).ready(function(){
+var counter = 1;
+var limit = 20;
+function addInput(divName){
+     if (counter == limit)  {
+          alert("You have reached the limit of adding " + counter + " inputs");
+     }
+     else {
+          var newdiv = document.createElement('div');
+          newdiv.innerHTML = `
+          <br><div class="ui input">
+             <input type="text" placeholder="Nom du champ" name='field_name'>
+           </div>
+          <div class="ui selection dropdown">
+          <input type="hidden" name="field_type">
+          <i class="dropdown icon"></i>
+          <div class="default text">Type`
+           + `</div>
+                <div class="menu">
+                  <div class="item" data-value="int">Entier</div>
+                  <div class="item" data-value="float">Flottant</div>
+                  <div class="item" data-value="date">Date</div>
+                  <div class="item"  data-value="string">ChaÎne de caractère</div>
+                  <div class="item"  data-value="text">Text</div>
+                  <div class="item"  data-value="boolean">Booléen</div>
+                </div>
+              </div>
+          `;
+          document.getElementById(divName).appendChild(newdiv);
+          counter++;
+          $('.ui.selection.dropdown')
+            .dropdown()
+          ;
 
-    var counter = 1;
-    var limit = 20;
-    function addInput(divName){
-         if (counter == limit)  {
-              alert("You have reached the limit of adding " + counter + " inputs");
-         }
-         else {
-              var newdiv = document.createElement('div');
-              newdiv.innerHTML = `
-              <br><div class="ui input">
-                 <input type="text" placeholder="Nom du champ" name='field_name'>
-               </div>
-              <div class="ui selection dropdown">
-              <input type="hidden" name="field_type">
-              <i class="dropdown icon"></i>
-              <div class="default text">Type`
-               + `</div>
-                    <div class="menu">
-                      <div class="item" data-value="int">Entier</div>
-                      <div class="item" data-value="float">Flottant</div>
-                      <div class="item" data-value="date">Date</div>
-                      <div class="item"  data-value="string">ChaÎne de caractère</div>
-                      <div class="item"  data-value="text">Text</div>
-                      <div class="item"  data-value="boolean">Booléen</div>
-                    </div>
-                  </div>
-              `;
-              document.getElementById(divName).appendChild(newdiv);
-              counter++;
-              $('.ui.selection.dropdown')
-                .dropdown()
-              ;
+     }
+}
 
-         }
+// form validation
+$('.addfeaturemodel')
+  .form({
+    fields: {
+      project:  {
+        identifier: 'project',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Veuillez choisir un projet'
+          }
+        ]
+      },
+      feature:  {
+        identifier: 'feature',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Veuillez entrer un nom pour ce type de signalement'
+          }
+        ]
+      },
+      geometry:  {
+        identifier: 'geometry',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Veuillez choisir un type de géometrie pour ce signalement'
+          }
+        ]
+      },
     }
+  })
+;
 
-    // form validation
-    $('.addfeaturemodel')
-      .form({
-        fields: {
-          project:  {
-            identifier: 'project',
-            rules: [
-              {
-                type   : 'empty',
-                prompt : 'Veuillez choisir un projet'
-              }
-            ]
-          },
-          feature:  {
-            identifier: 'feature',
-            rules: [
-              {
-                type   : 'empty',
-                prompt : 'Veuillez entrer un nom pour ce type de signalement'
-              }
-            ]
-          },
-          geometry:  {
-            identifier: 'geometry',
-            rules: [
-              {
-                type   : 'empty',
-                prompt : 'Veuillez choisir un type de géometrie pour ce signalement'
-              }
-            ]
-          },
+
+// add feature
+$('.ui.selection.dropdown.feature')
+  .dropdown()
+;
+$("#feature_type").change(function () {
+      name = $("input[name=feature]").val();
+
+      url = ''
+      $.ajax({
+        url: url,
+        type: 'GET',
+        data:{
+          name: name
+        },
+        success: function (result) {
+           $('#form_body').html(result);
+        },
+        error: function(err) {
+          console.log('Error occured', err);
         }
-      })
-    ;
-})
+      });
+
+});
