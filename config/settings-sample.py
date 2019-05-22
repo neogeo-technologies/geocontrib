9fe17e4ae923de22a020cad34243e42bbcd01199
bchartier <bchartier@neogeo.fr>
2019-05-22 17:40:21 +0200

    Mise à jour de settings pour exploitation de PostGIS

--- config/settings-sample.py
+++ config/settings-sample.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': '',
         'USER': '',
         'PASSWORD' : '',
