4989b8f2324ff7aa0cf8531d92bcbf19778c218e
Salima El Mokhtari <selmokhtari@neogeo.fr>
2019-06-25 10:08:46 +0200

    Ajout action suppression d'un projet

--- collab/actions.py
+++ collab/actions.py
@@ -1,8 +1,7 @@
 from collab.views.services.feature_services import delete_feature_table
 from collab.views.services.project_services import project_features_types
-from django.db import models
+from collab import models
 
-from django.shortcuts import get_object_or_404
 APP_NAME = __package__.split('.')[0]
 
 
@@ -10,14 +9,20 @@ def delete_project(modeladmin, request, queryset):
     """
         delete project and associated features tables
     """
-    for project in queryset:
-
+    for elt in queryset:
+        # remove the project
+        project = models.Project.objects.get(slug=elt.slug)
+        project.delete()
         # remove all project tables
-        feature_slug_list = project_features_types(APP_NAME, project.slug)
+        feature_slug_list = project_features_types(APP_NAME, elt.slug)
         for feature_type_slug in feature_slug_list:
-            deletion = delete_feature_table(APP_NAME, project.slug, feature_type_slug)
+            try:
+                deletion = delete_feature_table(APP_NAME, elt.slug, feature_type_slug)
+            except Exception as e:
+                print(deletion)
+                pass
+
         message = "le(s) projet(s) a/ont été supprimé "
         modeladmin.message_user(request, "%s." % message)
-        # remove the project
-        project.delete()
+
 delete_project.short_description = """Supprimer le(s) projet(s) et les tables des signalements"""
