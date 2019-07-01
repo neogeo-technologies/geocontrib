6ffb5efd48bbebee79c5447730902fbc0c89fb91
Salima El Mokhtari <selmokhtari@neogeo.fr>
2019-07-01 16:51:58 +0200

    Modification service export

--- collab/views/import_export.py
+++ collab/views/import_export.py
@@ -6,6 +6,8 @@ from collab.db_utils import commit_data
 from collab.views.services.feature_services import get_feature
 from collab.views.services.feature_services import get_feature_structure
 
+from collab.views.services.project_services import get_project_features
+from collab.views.services.project_services import project_features_types
 from collab.views.services.project_services import project_feature_type_fields
 from collab.views.services.user_services import authenticate_user
 
@@ -25,26 +27,29 @@ def export_data(request):
     """
         Export list of feature
     """
-    project_slug = 'test'
-    feature_type_slug = 'test123'
-    list_uuid = ['afd028a0-db2a-4b0b-a913-ef59f44f05ca']
-    res = []
-    if request.POST.get('project_slug', '') and request.POST.get('feature_type_slug', ''):
-        for feature_id in list_uuid:
-            feature = get_feature(APP_NAME, project_slug, feature_type_slug, feature_id)
-            # export only the features which have been published
-            if int(feature.get('status', '')) > 1:
-                feature['geom'] = json.loads(feature.get('geom', ''))
-                feature['status'] = STATUS[int(feature['status'])][1]
-                user = models.CustomUser.objects.get(id=feature.get('user_id', ''))
-                if user:
-                    feature['user'] = user.username
-                feature['link'] = """{domain}projet/{project_slug}/{feature_type}/{feature_id}""".format(
-                                  domain=request.build_absolute_uri('/'),
-                                  project_slug=project_slug,
-                                  feature_type=feature_type_slug,
-                                  feature_id=feature_id)
-                res.append(feature)
+    res = {}
+    if request.GET.get('project_slug', ''):
+        project_slug = request.GET.get('project_slug', '')
+        features_type = project_features_types(APP_NAME, project_slug)
+        for feature_type_slug in features_type:
+            features = get_project_features(APP_NAME, project_slug, feature_type_slug)
+            res_feature = []
+            for feature in features:
+                # export only the features which have been published
+                if int(feature.get('status', '')) > 1:
+                    feature['geom'] = json.loads(feature.get('geom', ''))
+                    feature['status'] = STATUS[int(feature['status'])][1]
+                    user = models.CustomUser.objects.get(id=feature.get('user_id', ''))
+                    if user:
+                        feature['user'] = user.username
+                    feature['link'] = """{domain}projet/{project_slug}/{feature_type}/{feature_id}""".format(
+                                      domain=request.build_absolute_uri('/'),
+                                      project_slug=project_slug,
+                                      feature_type=feature_type_slug,
+                                      feature_id=feature.get('feature_id', ''))
+
+                    res_feature.append(feature)
+            res[feature_type_slug] = res_feature
         return JsonResponse(res, safe=False)
     else:
         return JsonResponse({'erreur': 'le project slug et/ou le feature slug sont manquants'},
