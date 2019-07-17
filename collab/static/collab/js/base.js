29be981469f6787cf8a77aaef87a71bb4de33bab
Barbara ROMEO <bromeo@neogeo.fr>
2019-07-17 09:32:45 +0200

    dev front

---
+++ collab/static/collab/js/base.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  // INITIALISATION SEMANTIC UI COMPONENTS
+  // $('main .selection.dropdown').dropdown()
+  // $('main .ui.checkbox').checkbox()
+  // $('.message .close').on('click', function() {
+    // $(this).closest('.message').hide()
+  // })
+  updateCSSMinHeight()
+
+  $(window).resize(function(){
+    updateCSSMinHeight()
+  })
+
+  function updateCSSMinHeight() {
+    var body = parseInt($("body").css("height"),10)
+    var header = parseInt($("header").css("height"),10)
+    var footer = parseInt($("footer").css("height"),10)
+    var main = (body) - (header+footer)
+    $("main").css("min-height",main);
+  }
+
+  // $('.ui.sidebar').sidebar({
+  //     context: $('.ui.pushable.segment'),
+  //     transition: 'overlay'
+  // }).sidebar('attach events', '#mobile_item');
+
+})
