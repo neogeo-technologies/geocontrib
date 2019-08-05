9e0b95282c0d8201cf709b9d07065ff9967fb371
chakib <cbenhabib@neogeo.fr>
2019-08-05 14:00:58 +0200

    fix trigger decorator

--- collab/models.py
+++ collab/models.py
@@ -785,7 +785,7 @@ def disable_for_loaddata(signal_handler):
     """
     @wraps(signal_handler)
     def wrapper(*args, **kwargs):
-        if kwargs['raw']:
+        if kwargs.get('raw'):
             return
         signal_handler(*args, **kwargs)
     return wrapper
