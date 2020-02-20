942d503f2b7a0d9c0763139c86109a7aceef107f
bchartier <bchartier@neogeo.fr>
2020-02-20 17:34:47 +0100

    fix docker: improve settings

--- docker/urls.py
+++ docker/urls.py
@@ -3,21 +3,29 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 url_prefix = settings.URL_PREFIX
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls', namespace='api')),
     path('', include('geocontrib.urls', namespace='geocontrib')),
 ]
+
+# On ajoute les urls des différentes applis
+for app in settings.OUR_APPS:
+    urlpatterns.insert(
+        1,
+        path('{}/'.format(app), include('{}.urls'.format(app), namespace=app)),
+    )
 
 # add prefix to URL
 urlpatterns = [path(url_prefix, include(urlpatterns))]
 
 # static and media paths
-urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # error handlers
 handler404 = 'geocontrib.views.error.custom_404'
