641bbb266e276c44fcef0dbb9ca3156f78319e1f
bchartier <bchartier@neogeo.fr>
2020-08-07 15:54:16 +0200

    fix: use neogeo logo as default logo (one old file name remaining)

--- config_sample/settings.py
+++ config_sample/settings.py
@@ -173,7 +173,7 @@ DEFAULT_SENDING_FREQUENCY = config('DEFAULT_SENDING_FREQUENCY', default='never')
 APPLICATION_NAME = config('APPLICATION_NAME', default='GéoContrib')
 APPLICATION_ABSTRACT = config('APPLICATION_ABSTRACT',
                               default="Application de saisie d'informations géographiques contributive")
-LOGO_PATH = config('LOGO_PATH', default=os.path.join(MEDIA_URL, 'logo.png'))
+LOGO_PATH = config('LOGO_PATH', default=os.path.join(MEDIA_URL, 'logo-neogeo-rond.png'))
 FAVICON_PATH = config('FAVICON_PATH', default=os.path.join(MEDIA_URL, 'logo-neogeo-rond.png'))
 
 # Allowed formats for file attachments
