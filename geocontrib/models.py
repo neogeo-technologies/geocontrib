bce075cffb4997132c98f365c14cc6a7f5685748
Sébastien DA ROCHA <sebastien@da-rocha.net>
2020-12-17 16:37:12 +0100

    Remove commented code

--- geocontrib/models.py
+++ geocontrib/models.py
@@ -168,7 +168,6 @@ class Authorization(models.Model):
                 user_perms['can_create_feature_type'] = True
 
             # Visibilité des features archivés
-            # if user_rank >= project_arch_rank_min or project_arch_rank_min < 2:
             if user_rank >= project_arch_rank_min:
                 user_perms['can_view_archived_feature'] = True
 
