7b0af99d27b199f642f266bba67d4c42a0f0c8cf
hcharp <hcharpentier@neogeo.fr>
2021-05-05 16:26:21 +0200

    delete old tests_settings

--- docs/end-to-end/library/settings/tests_settings.py
+++
@@ -1,8 +0,0 @@
-def get_variables():
-    variables = {
-                "URL": "https://geocontrib.dev.neogeo.fr",
-                "ADMINURL": "https://geocontrib.dev.neogeo.fr/admin",
-                "SUPERUSERNAME": "CHANGE ME",
-                "SUPERUSERPASSWORD": "CHANGE ME",
-                }
-    return variables
