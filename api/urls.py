67cff5c5a8e80b86e8408dd9e85c131702d574b3
cbenhabib <cbenhabib@neogeo.fr>
2021-08-25 17:37:34 +0200

    feat/ init api feature-type

--- api/urls.py
+++ api/urls.py
@@ -5,33 +5,32 @@ from django.contrib.auth import views as auth_views
 
 from geocontrib.views import MyAccount
 
+# from api.views.project import ProjetImportFromGeoJSON
+from api.views.base_map import GetFeatureInfo
 from api.views.feature import ExportFeatureList
 from api.views.feature import FeatureSearch
 from api.views.feature import FeatureTypeView
 from api.views.feature import ProjectFeature
 from api.views.feature import ProjectFeatureTypes
-from api.views.project import ProjectView
-from api.views.project import ProjectThumbnailView
-from api.views.project import ProjectAuthorization
-# from api.views.project import ProjetImportFromGeoJSON
-from api.views.project import ProjectData
-from api.views.base_map import GetFeatureInfo
-from api.views.user import UserViewSet
-from api.views.login import MyAccountView
+from api.views.flat_pages import FlatPagesView
 from api.views.login import LoginView
 from api.views.login import LogoutView
+from api.views.login import MyAccountView
 from api.views.login import UserInfoView
-from api.views.flat_pages import FlatPagesView
-from api.views.user import UserLevelProjectView
 from api.views.misc import ImportTaskSearch
 from api.views.misc import ProjectComments
+from api.views.project import ProjectAuthorization
+from api.views.project import ProjectThumbnailView
+from api.views.project import ProjectView
+from api.views.user import UserLevelProjectView
+from api.views.user import UserViewSet
 
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectView, basename='projects')
-router.register(r'feature-type', FeatureTypeView, basename='projects')
+router.register(r'feature-types', FeatureTypeView, basename='feature-types')
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'import-tasks', ImportTaskSearch, basename='importtask')
 
@@ -52,28 +51,24 @@ urlpatterns = [
     path('flat-pages/', FlatPagesView.as_view(), name='help'),
 
     # Vues de gestion et d'édition des données métiers
-    path('user_level_project/', UserLevelProjectView.as_view(), name='user_level_project'),
-
     path(
-        'user/<slug:slug>',
-        ProjectData.as_view(), name='user'),
+        'user_level_project/',
+        UserLevelProjectView.as_view(),
+        name='user_level_project'),
     path(
-        'projet/<slug:slug>/project',
-        ProjectData.as_view(), name='project-data'),
-    path(
-        'project/<slug:slug>/thumbnail',
+        'projects/<slug:slug>/thumbnail/',
         ProjectThumbnailView.as_view(), name='project-thumbnail'),
     path(
-        'projet/<slug:slug>/utilisateurs',
+        'projet/<slug:slug>/utilisateurs/',
         ProjectAuthorization.as_view(), name='project-authorization'),
     path(
-        'projet/<slug:slug>/feature_types',
+        'projet/<slug:slug>/feature_types/',
         ProjectFeatureTypes.as_view(), name='project-feature-types'),
     path(
-        'projet/<slug:slug>/feature',
+        'projet/<slug:slug>/feature/',
         ProjectFeature.as_view(), name='project-feature'),
     path(
-        'projet/<slug:slug>/comments',
+        'projet/<slug:slug>/comments/',
         ProjectComments.as_view(), name='project-comments'),
     path(
         'projet/<slug:slug>/type-signalement/<slug:feature_type_slug>/export',
