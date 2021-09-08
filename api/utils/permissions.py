99a1a397d646863f6db894d395affb7796f68772
cbenhabib <cbenhabib@neogeo.fr>
2021-09-08 11:02:34 +0200

    fix permissions

--- api/utils/permissions.py
+++ api/utils/permissions.py
@@ -6,7 +6,7 @@ from geocontrib.models import Authorization
 class ProjectThumbnailPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
+        return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
