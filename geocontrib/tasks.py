e0736d771d2c45b4ece84719ef88df891897164b
cbenhabib <cbenhabib@neogeo.fr>
2021-09-13 09:28:14 +0200

    Celery beat geocontrib tasks:
    $ pip install django-celery-beat==2.2.1
    Add ''django_celery_beat',' to INSTALLED_APP
    $ python manage.py migrate
    $ python manage.py loaddata geocontrib/data/geocontrib_beat.json

--- geocontrib/tasks.py
+++ geocontrib/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 
 from geocontrib.models import ImportTask
 from geocontrib.utils.geojson import geojson_processing
+from django.core.management import call_command
 
 
 @shared_task()
@@ -11,3 +12,13 @@ def task_geojson_processing(import_task_id):
     except ImportTask.DoesNotExist:
         raise Exception("ImportTask {} not found".format(import_task_id))
     geojson_processing(import_task)
+
+
+@shared_task()
+def task_data_cleansing():
+    call_command('data_cleansing', 'all')
+
+
+@shared_task()
+def task_notify_subscribers():
+    call_command('notify_subscribers')
