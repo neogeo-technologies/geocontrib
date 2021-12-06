db360f1d83f0ed2c26cf32aa3fa9a2a89d5ebca2
Florent <florent@MacBook-Air-de-neogeo.local>
2021-12-06 12:30:11 +0100

    add ordering to project feature view

--- api/views/feature.py
+++ api/views/feature.py
@@ -123,7 +123,6 @@ class ProjectFeature(views.APIView):
     def get(self, request, slug):
         project = get_object_or_404(Project, slug=slug)
         features = Feature.handy.availables(request.user, project)
-
         title_contains = self.request.query_params.get('title__contains')
         if title_contains:
             features = features.filter(title__contains=title_contains)
@@ -132,6 +131,9 @@ class ProjectFeature(views.APIView):
         if title_icontains:
             features = features.filter(title__icontains=title_icontains)
         count = features.count()
+        ordering = self.request.query_params.get('ordering')
+        if ordering:
+            features = features.order_by(ordering)
         limit = self.request.query_params.get('limit')
         if limit:
             features = features[:int(limit)]
