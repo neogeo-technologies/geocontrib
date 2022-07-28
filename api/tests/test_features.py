dcf547df68effc72b461c1f982d1b8a8c44d1726
Leandro Almada <lalmada@neogeo.fr>
2022-07-28 10:02:57 +0000

    REDMINE_ISSUES-14347 | add is_mandatory in custom field feature type

--- api/tests/test_features.py
+++ api/tests/test_features.py
@@ -2,8 +2,6 @@ from django.core.management import call_command
 from django.urls import reverse
 import pytest
 
-from geocontrib.models.project import Project
-from geocontrib.models.user import UserLevelPermission
 from geocontrib.models.user import User
 from conftest import verify_or_create_json
 from conftest import verify_or_create
@@ -80,6 +78,7 @@ def test_projectfeature(api_client):
     features_url = reverse('api:project-feature', args=["1-aze"])
     result = api_client.get(f'{ features_url }')
     assert result.status_code == 200
+
     verify_or_create_json("api/tests/data/test_projectfeature_anon.json",
                           result.json(),
                           sorter=sort_simple_features_by_title
@@ -264,53 +263,3 @@ def test_feature_event(api_client):
     result = api_client.get(features_url)
     assert result.status_code == 200
     verify_or_create_json("api/tests/data/test_feature_events_empty.json", result.json())
-
-
-@pytest.mark.django_db
-@pytest.mark.freeze_time('2021-08-05')
-def test_feature_types_list(api_client):
-    call_command("loaddata", "geocontrib/data/perm.json", verbosity=0)
-    call_command("loaddata", "api/tests/data/test_features.json", verbosity=0)
-
-    features_url = reverse('api:feature-types-list')
-    # Ensure available even when not logged in
-    result = api_client.get(features_url)
-    assert result.status_code == 200
-    verify_or_create_json("api/tests/data/test_features_types_anon.json", result.json())
-
-
-    user = User.objects.get(username="admin")
-    api_client.force_authenticate(user=user)
-
-    data = {
-        'title': "New feature type",
-        'title_optional': False,
-        'geom_type': "point",
-        'color': "#000000",
-        'opacity': "0.5",
-        'project': '1-aze',
-        'customfield_set': None
-    }
-
-    # Test Can create feature type
-    result = api_client.post(features_url, data, format="json")
-    assert result.status_code == 201
-    verify_or_create_json("api/tests/data/test_features_types_create.json", result.json())
-
-    # To be able to edit the previous feature type
-    #data["slug"] = "3-new-feature-type"
-    data['title'] = "New feature type edited"
-
-    # Test Can edit feature type
-    features_url = reverse('api:feature-types-detail', args=['3-new-feature-type'])
-    result = api_client.put(features_url, data, format="json")
-    assert result.status_code == 200
-    # TODO ne marche pas, PUT retourne le vielle objet, pas le nouveau... mais il est bien inscrit en base
-    #verify_or_create_json("api/tests/data/test_features_types_edit.json", result.json())
-
-    features_url = reverse('api:feature-types-detail', args=['3-new-feature-type'])
-    result = api_client.get(features_url)
-    assert result.status_code == 200
-    verify_or_create_json("api/tests/data/test_features_types_edit.json", result.json())
-
-    # TODO ajouter test sur les customfields (ou les ajouter dans ces test
