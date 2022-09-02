3fad0ae94bd2258b803e9f30a5b9d95b9f5c1da7
Sébastien DA ROCHA <sebastien@da-rocha.net>
2022-09-02 10:55:49 +0200

    #14427 fix nom endpoint liste des listes pre enregistrées

--- api/tests/test_pre_recorded_values.py
+++ api/tests/test_pre_recorded_values.py
@@ -7,7 +7,7 @@ from conftest import verify_or_create_json
 
 @pytest.mark.django_db
 def test_list_prerecorded_values_list(api_client):
-    prv_url = reverse('api:list-values')
+    prv_url = reverse('api:prerecorded-list-values')
     result = api_client.get(prv_url)
     assert result.status_code == 200
 
@@ -27,7 +27,7 @@ def test_list_prerecorded_values_list(api_client):
 @pytest.mark.django_db
 def test_get_prerecorded_values_list(api_client):
     call_command("loaddata", "api/tests/data/test_pre_recorded_values.json", verbosity=0)
-    prv_url = reverse('api:list-values', args=["Toulouse Metropole"])
+    prv_url = reverse('api:prerecorded-list-values', args=["Toulouse Metropole"])
     # Ensure no parameters Fails
     result = api_client.get(prv_url)
 
--- api/urls.py
+++ api/urls.py
@@ -150,11 +150,11 @@ urlpatterns = [
         GetFeatureInfo.as_view(), name='proxy'),
 
     path(
-        'list-values/',
-        PreRecordedValuesView.as_view(), name='list-values'),
+        'prerecorded-list-values/',
+        PreRecordedValuesView.as_view(), name='prerecorded-list-values'),
     path(
-        'list-values/<str:name>/',
-        PreRecordedValuesView.as_view(), name='list-values'),
+        'prerecorded-list-values/<str:name>/',
+        PreRecordedValuesView.as_view(), name='prerecorded-list-values'),
 ]
 
 
