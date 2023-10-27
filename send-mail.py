46701789a2e7f757d41a2f401ec0d3657815a8f3
Timothée Poussard <tpoussard@neogeo.fr>
2023-10-27 18:13:04 +0200

    add debug logs

--- send-mail.py
+++ send-mail.py
@@ -1,7 +1,9 @@
 import os
 import django
 import argparse
+import logging
 from django.core.mail import send_mail
+
 
 """
 Script d'envoi de mails pour Geocontrib
@@ -35,26 +37,47 @@ Auteur : Timothée Poussard
 Date : 27/10/2023
 """
 
+# Configuration du logging
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+
 # Initialisation de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
+try:
+    django.setup()
+    logger.debug("Django initialized successfully.")
+except Exception as e:
+    logger.error(f"Error initializing Django: {e}")
+    exit(1)
 
 def main():
+    logger.debug("Starting main function...")
     parser = argparse.ArgumentParser(description='Send an email using Django.')
     parser.add_argument('--to', required=True, help='Recipient email address.')
     parser.add_argument('--subject', required=True, help='Email subject.')
     parser.add_argument('--message', default='', help='Email message.')
 
     args = parser.parse_args()
+    logger.debug(f"Arguments parsed: to={args.to}, subject={args.subject}, message={args.message}")
 
-    # Utilisez la fonction send_mail de Django pour envoyer l'e-mail
-    send_mail(
-        args.subject,
-        args.message,
-        None,  # En laissant l'expéditeur à None, Django utilisera DEFAULT_FROM_EMAIL comme expéditeur par défaut
-        [args.to],
-        fail_silently=False,
-    )
+    logger.debug("Preparing to send email...")
+    try:
+        send_mail(
+            args.subject,
+            args.message,
+            None,  # En laissant l'expéditeur à None, Django utilisera DEFAULT_FROM_EMAIL
+            [args.to],
+            fail_silently=False,
+        )
+        logger.info(f"Email sent successfully to {args.to} with subject '{args.subject}'.")
+    except Exception as e:
+        # Vérifier si l'erreur est "Relay access denied"
+        if 'Relay access denied' in str(e):
+            logger.error("Failed to send email due to SMTP relay access denied. Check your SMTP server configuration.")
+        else:
+            logger.error(f"Error sending email: {e}")
+
+    logger.debug("Exiting main function...")
 
 if __name__ == "__main__":
     main()
