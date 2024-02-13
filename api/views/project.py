2975b4f362ff7e136b0d67cb9a96da58bd09d593
Timothée Poussard <tpoussard@neogeo.fr>
2024-02-13 17:35:31 +0100

    add endpoint for project attribute list

--- api/views/project.py
+++ api/views/project.py
@@ -17,8 +17,9 @@ from rest_framework.views import APIView
 from rest_framework import mixins
 
 from api.serializers import ProjectDetailedSerializer
-from api.serializers.project import ProjectCreationSerializer
-from api.serializers.project import ProjectAuthorizationSerializer
+from api.serializers import ProjectAttributeSerializer
+from api.serializers import ProjectCreationSerializer
+from api.serializers import ProjectAuthorizationSerializer
 from api.utils.permissions import ProjectPermission
 from api.utils.validators import validate_image_file
 from api.utils.filters import AuthorizationLevelCodenameFilter
@@ -34,6 +35,7 @@ from geocontrib.models import Project
 from geocontrib.models import Subscription
 from geocontrib.models import FeatureType
 from geocontrib.models import BaseMap
+from geocontrib.models import ProjectAttribute
 
 
 User = get_user_model()
@@ -297,3 +299,7 @@ class ProjectSubscription(APIView):
                 obj.save()
         data = {'is_suscriber': Subscription.is_suscriber(request.user, project)}
         return Response(data=data, status=200)
+
+class ProjectAttributeListView(generics.ListAPIView):
+    queryset = ProjectAttribute.objects.all()
+    serializer_class = ProjectAttributeSerializer
