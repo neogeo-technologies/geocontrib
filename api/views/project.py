e9dea4aab915235eba00b37aa8c02c2194a79cb3
Timothée Poussard <tpoussard@neogeo.fr>
2024-08-09 15:06:50 +0200

    feat: classify project view

--- api/views/project.py
+++ api/views/project.py
@@ -6,6 +6,7 @@ from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import Http404
 from django.conf import settings
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import exceptions
 from rest_framework import filters
 from rest_framework import permissions
@@ -70,7 +71,7 @@ class ProjectView(viewsets.ModelViewSet):
             return ProjectDetailedSerializer
         if self.action == 'create':
             return ProjectCreationSerializer
-        if self.action in ['update', 'partial_update'] :
+        if self.action in ['update', 'partial_update']:
             return ProjectCreationSerializer
         return ProjectDetailedSerializer
 
@@ -82,6 +83,10 @@ class ProjectView(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
 
+    @swagger_auto_schema(
+        operation_summary="Delete project",
+        tags=["projects"]
+    )
     def destroy(self, request, *args, **kwargs):
         slug = self.kwargs.get('slug')
         project = get_object_or_404(Project, slug=slug)
@@ -90,6 +95,10 @@ class ProjectView(viewsets.ModelViewSet):
             return super().destroy(request, *args, **kwargs)
         raise exceptions.PermissionDenied
 
+    @swagger_auto_schema(
+        operation_summary="Update project",
+        tags=["projects"]
+    )
     def update(self, request, *args, **kwargs):
         slug = self.kwargs.get('slug')
         project = get_object_or_404(Project, slug=slug)
@@ -98,40 +107,64 @@ class ProjectView(viewsets.ModelViewSet):
             return super().update(request, *args, **kwargs)
         raise exceptions.PermissionDenied
 
+    @swagger_auto_schema(
+        operation_summary="Partially update a project",
+        tags=["projects"]
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-class ProjectTypesView(
-        mixins.ListModelMixin,
-        viewsets.GenericViewSet):
-    """
-    Get all project-types
-    """
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    @swagger_auto_schema(
+        operation_summary="Create a project",
+        tags=["projects"]
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
 
-    queryset = Project.objects.filter(is_project_type=True).order_by('-created_on')
+    @swagger_auto_schema(
+        operation_summary="List projects",
+        tags=["projects"]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
-    serializer_class = ProjectDetailedSerializer
+    @swagger_auto_schema(
+        operation_summary="Retrieve project",
+        tags=["projects"]
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectDuplicate(APIView):
-    http_method_names = ['post', ]
+    """
+    Duplicate a project.
+
+    This endpoint allows authenticated users to create a duplicate of an existing project,
+    including related features such as thumbnails, base maps, and authorizations, depending
+    on settings.
+    """
+    
+    http_method_names = ['post']
     serializer_class = ProjectCreationSerializer
-    permission_classes = (
-        permissions.IsAuthenticated,
-    )
+    permission_classes = (permissions.IsAuthenticated,)
 
     PROJECT_COPY_RELATED = getattr(settings, 'PROJECT_COPY_RELATED', {})
 
+    @swagger_auto_schema(operation_summary="Duplicate an existing project")
     def post(self, request, slug):
+        # Fetch the template project to be duplicated
         project_template = get_object_or_404(Project, slug=slug)
 
+        # Validate and deserialize input data
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         if serializer.is_valid():
+            # Save the new project instance
             instance = serializer.save(creator=request.user)
 
+            # Duplicate related project data
             self._set_creator(instance)
             self._duplicate_project_thumbnail(instance, project_template)
             self._duplicate_project_related_sets(instance, project_template)
@@ -144,19 +177,21 @@ class ProjectDuplicate(APIView):
         else:
             data = serializer.errors
             status = 400
+        
+        # Return the response with the appropriate status
         return Response(data=data, status=status)
 
     def _duplicate_project_related_sets(self, instance, project_template):
         copy_feature_types = self.PROJECT_COPY_RELATED.get('FEATURE_TYPE', False)
         if project_template and isinstance(project_template, Project) and copy_feature_types:
             for feature_type in project_template.featuretype_set.all():
-                # Pour manipuler une copie immuable
+                # Retrieve a fresh copy of the feature type
                 legit_feature_type = FeatureType.objects.get(pk=feature_type.pk)
-                feature_type.pk = None
+                feature_type.pk = None  # Clear the primary key to create a new instance
                 feature_type.project = instance
                 feature_type.save()
                 for custom_field in legit_feature_type.customfield_set.all():
-                    custom_field.pk = None
+                    custom_field.pk = None  # Clear the primary key for the new custom field
                     custom_field.feature_type = feature_type
                     custom_field.save()
 
@@ -165,32 +200,28 @@ class ProjectDuplicate(APIView):
         if project_template and isinstance(project_template, Project) and copy_related:
             for base_map in project_template.basemap_set.all():
                 legit_base_map = BaseMap.objects.get(pk=base_map.pk)
-                base_map.pk = None
+                base_map.pk = None  # Clear the primary key to create a new instance
                 base_map.project = instance
                 base_map.save()
                 for ctx_layer in legit_base_map.contextlayer_set.all():
-                    ctx_layer.pk = None
+                    ctx_layer.pk = None  # Clear the primary key for the new context layer
                     ctx_layer.base_map = base_map
                     ctx_layer.save()
 
     def _duplicate_project_thumbnail(self, instance, project_template):
         copy_related = self.PROJECT_COPY_RELATED.get('THUMBNAIL', True)
         if project_template and isinstance(project_template, Project) and copy_related:
-            original_thumbnail = project_template.thumbnail # get original_thumbnail object
-            new_thumbnail_name = original_thumbnail.name.split('/')[-1] # get only the file name
-            new_thumbnail_file = original_thumbnail.file.read() # copy the file
-            new_file = ContentFile(new_thumbnail_file, new_thumbnail_name) # create an new object file
-            instance.thumbnail = new_file # add the thumbnail to the instance of the duplicated project
+            original_thumbnail = project_template.thumbnail  # Get the original thumbnail object
+            new_thumbnail_name = original_thumbnail.name.split('/')[-1]  # Extract the file name
+            new_thumbnail_file = original_thumbnail.file.read()  # Copy the file content
+            new_file = ContentFile(new_thumbnail_file, new_thumbnail_name)  # Create a new file object
+            instance.thumbnail = new_file  # Assign the new thumbnail to the instance
             instance.save()
 
     def _duplicate_project_authorization(self, instance, project_template):
         """
-        Un signale est deja en place pour appliquer les permissions initiales
-        à la creation d'un projet:
-        - createur = rank 4
-        - autres = rank 1
-        Ici on ecrase ces permissions initiales avec celles du projet type parent
-        à l'exclusion de la permission relative au créateur de l'instance courante
+        Duplicates the authorization levels from the original project to the new 
+        project instance, except for the creator of the current instance.
         """
         copy_related = self.PROJECT_COPY_RELATED.get('AUTHORIZATION', False)
         if project_template and isinstance(project_template, Project) and copy_related:
@@ -200,6 +231,9 @@ class ProjectDuplicate(APIView):
                 auth.save()
 
     def _set_creator(self, instance):
+        """
+        Assigns the current user as the creator of the duplicated project.
+        """
         instance.creator = self.request.user
         return instance
 
